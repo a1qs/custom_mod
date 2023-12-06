d88fd3511f472db321878d14cc2008e2c3a499e2
a1qs <ankiller2007.09.20@gmail.com>
2023-12-06 22:48:50 +0100

    mod mixin, mod init & icon

---
+++ src/main/java/io/github/a1qs/CustomMod.java
@@ -0,0 +1,19 @@
+package io.github.a1qs;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod name as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("Custom Mod");
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		LOGGER.info("we pray that it doenst crash");
+	}
+}

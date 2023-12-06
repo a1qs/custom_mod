d537f8d94570f8c4d0d8f18256f32be05f4e064c
a1qs <92255041+a1qs@users.noreply.github.com>
2023-12-06 21:49:33 +0100

    Update CustomMod.java

--- src/main/java/com/example/example_mod/ExampleMod.java
+++ src/main/java/com/example/example_mod/ExampleMod.java
@@ -9,10 +9,11 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Custom Mod");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		LOGGER.info("we pray that it doenst crash")
 	}
 }

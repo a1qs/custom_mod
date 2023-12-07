98894fdc498febbb41125ead30bfd3f11b172179
a1qs <ankiller2007.09.20@gmail.com>
2023-12-07 20:08:44 +0100

    add items, lang files & license

--- src/main/java/io/github/a1qs/CustomMod.java
+++ src/main/java/io/github/a1qs/CustomMod.java
@@ -1,9 +1,17 @@
 package io.github.a1qs;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static io.github.a1qs.CustomItems.*;
 
 public class CustomMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -11,9 +19,25 @@ public class CustomMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Custom Mod");
 
+
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
-		LOGGER.info("we pray that it doenst crash");
+		LOGGER.info("Hello world from {}!", mod.metadata().name());
+
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "example_item"), EXAMPLE_ITEM);
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "item_better"), ITEM_BETTER);
+
+		Registry.register(Registries.ITEM_GROUP, new Identifier("tutorial", "test_group"), ITEM_GROUP);
+
+
 	}
+	private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(EXAMPLE_ITEM))
+			.name(Text.translatable("itemGroup.tutorial.test_group"))
+			.entries((context, entries) -> {
+				entries.addItem(EXAMPLE_ITEM);
+				entries.addItem(ITEM_BETTER);
+			})
+			.build();
+
 }
